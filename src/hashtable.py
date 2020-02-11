bcf7491c33caaeadafec3f27bee57b562255c35c
Diana Grams <deegrams221@gmail.com>
2020-02-11 14:52:32 -0700

    day2 - completed 'retrieve' in hashtable.py

--- src/hashtable.py
+++ src/hashtable.py
@@ -16,7 +16,6 @@ class HashTable:
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
 
-
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
@@ -49,7 +48,8 @@ class HashTable:
         '''
         # pass
 
-         # compute index of key
+    # Day 1 Assignment:
+        # compute index of key
         index = self._hash_mod(key)
         # for loop, i in range length of storage
         for i in range(len(self.storage)):
@@ -62,15 +62,24 @@ class HashTable:
                 print(f"\nWARNING: Not empty.")
                 return None
 
-# '''
-# Notes from Lecture 2/11
-# '''
-#         index = self._hash_mod(key)
-#         if self.storage[index] is not None:
-#             print(f"WARNING:  Collision has occured at {index}")
-#         else:
-#             self.storage[index] = (key, value)
-#         return
+    # '''
+    # Notes from Lecture 2/11
+    # '''
+        # index = self._hash_mod(key)
+        # if self.storage[index] is not None:
+        #     print(f"\nWARNING:  Collision has occured at {index}")
+        # else:
+        #     self.storage[index] = (key, value)
+        # return
+
+    # Day 2 Assignment:
+        # index = self._hash_mod(key)
+        # if self.storage[index] is not None:
+        # # Add a Linked List to account for collision
+            
+        # else:
+        #     self.storage[index] = (key, value)
+        # return
 
 
     def remove(self, key):
@@ -81,27 +90,39 @@ class HashTable:
         '''
         # pass
 
+    # Day 1 Assignment:
         # compute index of key
-        index = self._hash_mod(key)
-        # if the index of storage is None
-        if self.storage[index] is None:
-            # just print for now, collision handling tomorrow
-            print(f"WARNING: Key not found.")
-            return None
-        self.storage[index] = None
+        # index = self._hash_mod(key)
+        # # if the index of storage is None
+        # if self.storage[index] is None:
+        #     # just print for now, collision handling tomorrow
+        #     print(f"WARNING: Key not found.")
+        #     return None
+        # self.storage[index] = None
 
-# '''
-# Notes from Lecture 2/11
-# '''
-#         index = self._hash_mod(key)
-#         if self.storage[index] is not None:
-#             if self.storage[index][0] == key:
-#                 self.storage[index] = None
-#             else:
-#                 print(f"WARNING:  Collision has occured at {index}")
-#         else:
-#             print(f"Warning key ({key}) not found.")
-#         return
+    # '''
+    # Notes from Lecture 2/11
+    # '''
+        # index = self._hash_mod(key)
+        # if self.storage[index] is not None:
+        #     if self.storage[index][0] == key:
+        #         self.storage[index] = None
+        #     else:
+        #         print(f"WARNING:  Collision has occured at {index}")
+        # else:
+        #     print(f"Warning key ({key}) not found.")
+        # return
+
+    # Day 2 Assignment:
+        # index = self._hash_mod(key)
+        # if self.storage[index] is not None:
+        #     if self.storage[index][0] == key:
+        #         self.storage[index] = None
+        #     else:
+        #         print(f"WARNING:  Collision has occured at {index}")
+        # else:
+        #     print(f"Warning key ({key}) not found.")
+        # return
 
     def retrieve(self, key):
         '''
@@ -111,28 +132,40 @@ class HashTable:
         '''
         # pass
 
+    # Day 1 Assignment:
         # compute index of key
-        index = self._hash_mod(key)
-        # if storage of index is not None
-        if self.storage[index] != None:
-            return self.storage[index]
-        else:
-            # just print for now, collision handling tomorrow
-            print(f"WARNING: Key doesn't match.")
-            return None
+        # index = self._hash_mod(key)
+        # # if storage of index is not None
+        # if self.storage[index] != None:
+        #     return self.storage[index]
+        # else:
+        #     # just print for now, collision handling tomorrow
+        #     print(f"WARNING: Key doesn't match.")
+        #     return None
 
-# '''
-# Notes from Lecture 2/11
-# '''
-#         index = self._hash_mod(key)
-#         if self.storage[index] is not None:
-#             if self.storage[index][0] == key:
-#                 return self.storage[index][1]
-#             else:
-#                 print(f"WARNING:  Collision has occured at {index}")
-#         else:
-#             return None
-#         return
+    # '''
+    # Notes from Lecture 2/11
+    # '''
+        # index = self._hash_mod(key)
+        # if self.storage[index] is not None:
+        #     if self.storage[index][0] == key:
+        #         return self.storage[index][1]
+        #     else:
+        #         print(f"WARNING:  Collision has occured at {index}")
+        # else:
+        #     return None
+        # return
+
+    # Day 2 Assignment:
+        index = self._hash_mod(key)
+        # loop
+        for i in range(self.capacity):
+            index_i = self.storage[index]
+            # check if index is not None and check if index[0] is the key
+            if index_i is not None and index_i[0] == key:
+                return index_i[1]
+        return None
+
 
     def resize(self):
         '''
@@ -142,6 +175,7 @@ class HashTable:
         '''
         # pass
 
+    # Day 1 Assignment:
         # Doubles the capacity of the hash table & rehash all key/value pairs.
         self.capacity *= 2
         new_storage = [None] * self.capacity
@@ -155,15 +189,23 @@ class HashTable:
         # reassign the referance (change the pointer)
         self.storage = new_storage
 
-# '''
-# Notes from Lecture 2/11
-# '''
-#         old_storage = self.storage
-#         self.capacity *= 2
-#         self.storage = [None] * self.capacity
+    # '''
+    # Notes from Lecture 2/11
+    # '''
+        # old_storage = self.storage
+        # self.capacity *= 2
+        # self.storage = [None] * self.capacity
 
-#         for item in old_storage:
-#             self.insert(item[0], item[1])
+        # for item in old_storage:
+        #     self.insert(item[0], item[1])
+
+    # Day 2 Assignment:
+        # old_storage = self.storage
+        # self.capacity *= 2
+        # self.storage = [None] * self.capacity
+
+        # for item in old_storage:
+        #     self.insert(item[0], item[1])
 
 
 
@@ -179,9 +221,9 @@ if __name__ == "__main__":
 # '''
 # Notes from Lecture 2/11
 # '''
-#     ht1.insert("key1", "hello")
-#     ht1.insert("unicorn", "goodbye")
-#     ht1.remove("key1")
+    # ht1.insert("key1", "hello")
+    # ht1.insert("unicorn", "goodbye")
+    # ht1.remove("key1")
 
     # Test storing beyond capacity
     print(ht.retrieve("line_1"))
